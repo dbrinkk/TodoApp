623cafcd8ee94c22b1ef18e6c0f9e10cef5db057
dbrinkk <danbrinkk@gmail.com>
2024-06-23 15:52:00 -0400

    basic file structure

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+// func main() {
+// 	// create store layer
+// 	storeLayer := storelayer.New()
+
+// 	// create app layer
+// 	appLayer := applayer.New(storeLayer)
+
+// 	// create http layer
+// 	api := httplayer.New(appLayer)
+
+// 	api.Engage()
+// }
